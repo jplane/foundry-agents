96134bcb3c8f4cb6a2048f7d408ef1d4d51b6761
Josh Lane <jolan@microsoft.com>
2025-03-25 11:42:31 +0000

    updated devcontainer setup for az funcs

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# install azure functions core tools
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs 2>/dev/null)-prod $(lsb_release -cs 2>/dev/null) main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get update
+sudo apt-get install azure-functions-core-tools-4
+
+# install python packages
+pip install --user -r src/notebooks/requirements.txt
+pip install --user -r src/api/requirements.txt
